6bc71cbcd18fd48de8e257b3f86b7a057ccfde2d
Казбеев Олег <kazbeev@gmail.com>
2020-09-08 18:20:48 +0700

    1) An selection menu for choosing of specialty was added to vacancy edit page; 2) Feedbacks and their amount were added to vacancy edit page. Also feedbacks amount was added to vacancy list page; 3) Successes alerts for company edit and vacancy edit pages were added.

--- companies/views.py
+++ companies/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import View
+from django.contrib import messages
 
 from companies.forms import CompanyForm
 from companies.models import Company
 from vacancies.forms import VacancyForm
-from vacancies.models import Vacancy, Specialty
+from vacancies.models import Vacancy, Specialty, Feedback
 
 
 class UserCompanyView(View):
@@ -22,6 +23,7 @@ class UserCompanyView(View):
         company_form = CompanyForm(data=request.POST, instance=user_company)
         if company_form.is_valid():
             company_form.save()
+            messages.success(request, 'Компания обновлена', extra_tags='alert alert-info')
         else:
             print(company_form.errors)
         return redirect('/mycompany')
@@ -31,8 +33,14 @@ class UserCompanyVacanciesView(View):
     def get(self, request):
         user_company = Company.objects.get(owner=request.user)
         user_vacancies = Vacancy.objects.filter(company=user_company)
+        vacancies_and_amount_of_feedbacks = []
+
+        for vacancy in user_vacancies:
+            feedbacks_amount = Feedback.objects.filter(vacancy=vacancy).count
+            vacancies_and_amount_of_feedbacks.append([vacancy, feedbacks_amount])
+
         return render(request,  'companies/vacancy-list.html',
-                      {'vacancies': user_vacancies})
+                      {'vacancies': vacancies_and_amount_of_feedbacks})
 
     def post(self, request):
         user_company = get_object_or_404(Company, owner=request.user)
@@ -45,14 +53,20 @@ class UserCompanyVacanciesView(View):
 class UserCompanyVacancyView(View):
     def get(self, request, vacancy_id):
         user_vacancy = Vacancy.objects.get(id=vacancy_id)
+        feedbacks = Feedback.objects.filter(vacancy=user_vacancy)
+        specialties = Specialty.objects.all()
+        amount_of_feedback = feedbacks.count
         return render(request, 'companies/vacancy-edit.html',
-                      {"vacancy": user_vacancy})
+                      {"vacancy": user_vacancy,
+                       "feedbacks": [feedbacks, amount_of_feedback],
+                       'specialties': specialties})
 
     def post(self, request, vacancy_id):
         vacancy = Vacancy.objects.get(id=vacancy_id)
         vacancy_form = VacancyForm(data=request.POST, instance=vacancy)
         if vacancy_form.is_valid():
             vacancy_form.save()
+            messages.success(request, "Вакансия обновлена", extra_tags='alert alert-info')
         else:
             print(vacancy_form.errors)
         return redirect(f'/mycompany/vacancies/{vacancy_id}')
